e86e19d197f33b7f1a5fb63cd214a14c9737f80c
Wannes Meert <wannes.meert@cs.kuleuven.be>
2018-03-23 17:18:14 +0100

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,9 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+
+here = os.path.abspath(os.path.dirname(__file__))
+sys.path.insert(0, os.path.join(here, '..'))
 
 
 # -- General configuration ------------------------------------------------
