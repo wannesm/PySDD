07b6825a49aae57e63334fd37ca81fac4e724e94
Wannes Meert <wannes.meert@cs.kuleuven.be>
2018-03-28 23:28:34 +0200

    Update

--- pysdd/wmcstochastic.py
+++ pysdd/wmcstochastic.py
@@ -12,6 +12,29 @@ from .sdd import WmcManager
 
 
 class WmcStochastic(WmcManager):
+    """Weighted Model Counting using Stochastic Computing.
+
+    This class is a demonstration how stochastic computing [1,2] could be used for weighted model counting
+    given an SDD datastructure. The basic concept is that instead of propagating floating point numbers
+    through the SDD, only True and False samples are passed through. Each leaf generates samples from
+    a Bernouilli distribution with as parameter the probability associated, the samples collected at the
+    root node then represent a Bernouilli distribution with as mean the Weighted Model Count.
+
+    The main change is that a conjunction or multiplication is replaced with an AND gate and a
+    disjunction or addition is replaced with a MUX gate. Since a MUX gate rescales the result of
+    the addition back to the range [0,1], we need to keep track of scaling factors to tune the MUX
+    gates in the network.
+
+    [1] Von Neumann, John. "Probabilistic logics and the synthesis of reliable organisms from unreliable
+    components." Automata studies 34 (1956): 43-98.
+    [2] A. Alaghi and J. P. Hayes. Computing with randomness. IEEE Spectrum, (3), March 2018.
+    """
+
+    def __init__(self, node, log_mode=1):
+        super().__init__(node, log_mode)
+        self.scalings = dict()
+        self.or_cumweights = dict()
+        self.compute_scalings()
 
     def propagate_normal(self):
         return self.depth_first_normal(self.node)
@@ -36,56 +59,84 @@ class WmcStochastic(WmcManager):
         return rvalue
 
     def propagate(self, bitlength=100):
+        nb_pos, nb_neg, scaling = self.propagate_counts(bitlength)
+        return (nb_pos / (nb_pos + nb_neg)) * scaling
+
+    def propagate_counts(self, bitlength=100):
         nb_pos, nb_neg = 0, 0
-        scaling = 0
+        scaling = self.scalings[self.node.id]
         for i in range(bitlength):
-            value, scaling = self.depth_first(self.node)
-            if value:
+            if self.counting_df(self.node):
                 nb_pos += 1
             else:
                 nb_neg += 1
-        return (nb_pos / (nb_pos + nb_neg)) * scaling
+        return nb_pos, nb_neg, scaling
 
-    def depth_first(self, node):
+    def counting_df(self, node):
+        """Depth-first counting."""
         # TODO: This ignores smoothing
         if node.is_decision():
-            rvalues = []
             rcounts = []
             for prime, sub in node.elements():
                 # Conjunction
-                pw, pc = self.depth_first(prime)
-                sw, sc = self.depth_first(sub)
-                result_value = pw & sw
-                result_count = pc * sc
-                # Disjunction
-                rvalues.append(result_value)
+                count_p = self.counting_df(prime)
+                count_s = self.counting_df(sub)
+                result_count = count_p & count_s
                 rcounts.append(result_count)
-            rcount = sum(rcounts)
+            # Disjunction
+            cumweights = self.or_cumweights[node.id]
             randval = random.random()
             choice = None
-            for i, cw in enumerate(accumulate(w / rcount for w in rcounts)):
+            for i, cw in enumerate(cumweights):
                 if cw > randval:
                     choice = i
                     break
             if choice is None:
                 choice = len(rcounts) - 1
-            # print(randval, rcounts, choice)
-            rvalue = (rvalues[choice], rcount)
+            return rcounts[choice]
         elif node.is_true():
-            rvalue = (True, 1)
+            return True
         elif node.is_false():
-            rvalue = (False, 1)
+            return False
         elif node.is_literal():
             w = self.literal_weight(node)
             if w > 1.0:
                 raise Exception(f"Stochastic WMC expects probabilities as weights, got {w} for {node}.")
             randval = random.random()
             if randval < w:
-                rvalue = (True, 1)
+                return True
             else:
-                rvalue = (False, 1)
-            # print(f'literal {node:<11}: weight={w}, random={randval:.3f}, result={rvalue}')
+                return False
         else:
             raise Exception(f"Unknown node type: {node}")
-        return rvalue
 
+    def compute_scalings(self):
+        self.compute_scalings_df(self.node)
+
+    def compute_scalings_df(self, node):
+        """Depth-first scaling."""
+        # TODO: This ignores smoothing
+        if node.is_decision():
+            scalings = []
+            for prime, sub in node.elements():
+                # Conjunction
+                scaling_p = self.compute_scalings_df(prime)
+                scaling_s = self.compute_scalings_df(sub)
+                result_scaling = scaling_p * scaling_s
+                scalings.append(result_scaling)
+            # Disjunction
+            total_scaling = sum(scalings)
+            self.or_cumweights[node.id] = list(accumulate(s / total_scaling for s in scalings))
+            self.scalings[node.id] = total_scaling
+            return total_scaling
+        elif node.is_true():
+            self.scalings[node.id] = 1
+            return 1
+        elif node.is_false():
+            self.scalings[node.id] = 1
+            return 1
+        elif node.is_literal():
+            self.scalings[node.id] = 1
+            return 1
+        else:
+            raise Exception(f"Unknown node type: {node}")
--- tests/test_wmcstochastic.py
+++ tests/test_wmcstochastic.py
@@ -2,6 +2,7 @@ from pysdd.sdd import SddManager, Vtree, WmcManager
 from pysdd.wmcstochastic import WmcStochastic
 import pytest
 from array import array
+import random
 
 
 def test_wmc1(verbose=False):
@@ -10,8 +11,9 @@ def test_wmc1(verbose=False):
     a, b, c, d = [sdd.literal(i) for i in range(1, 5)]
     # formula = (a & b) | c
     formula = (a & b) | (b & c) | (c & d)
-    with open("sdd.dot", "w") as out:
-        print(formula.dot(), file=out)
+    if verbose:
+        with open("sdd.dot", "w") as out:
+            print(formula.dot(), file=out)
 
     #                     -d   -c   -b   -a   a    b    c    d
     weights = array('d', [0.8, 0.7, 0.6, 0.5, 0.5, 0.4, 0.3, 0.2])
@@ -32,5 +34,41 @@ def test_wmc1(verbose=False):
     # assert wmc_result == pytest.approx(wmcs_result)
 
 
+def test_wmc2(verbose=False):
+    vtree = Vtree(var_count=4, var_order=[2, 1, 4, 3], vtree_type="balanced")
+    sdd = SddManager.from_vtree(vtree)
+    a, b, c, d = [sdd.literal(i) for i in range(1, 5)]
+    # formula = (a & b) | c
+    formula = (a & b) | (b & c) | (c & d)
+
+    #                     -d   -c   -b   -a   a    b    c    d
+    weights = array('d', [0.8, 0.7, 0.6, 0.5, 0.5, 0.4, 0.3, 0.2])
+
+    # Normal WMC
+    wmc = WmcManager(formula, log_mode=False)
+    wmc.set_literal_weights_from_array(weights)
+    wmc_result = wmc.propagate()
+
+    # Stochastic WMC
+    wmcs = WmcStochastic(formula, log_mode=False)
+    wmcs.set_literal_weights_from_array(weights)
+    nb_pos, nb_neg, scaling = 0, 0, 1
+    wmcs_results = []
+    iterations = 1000
+    for i in range(iterations):
+        random.seed()
+        nb_pos_i, nb_neg_i, scaling_i = wmcs.propagate_counts(bitlength=10)
+        nb_pos += nb_pos_i
+        nb_neg += nb_neg_i
+        scaling = scaling_i
+        wmcs_results.append((nb_pos / (nb_pos + nb_neg)) * scaling)
+    print(wmcs_results[-1])
+    if verbose:
+        import matplotlib.pyplot as plt
+        plt.plot([i*10 for i in range(iterations)], [wmc_result]*iterations)
+        plt.plot([i*10 for i in range(iterations)], wmcs_results)
+        plt.savefig("stochastic_wmc.png")
+
+
 if __name__ == "__main__":
-    test_wmc1(verbose=True)
+    test_wmc2(verbose=True)
