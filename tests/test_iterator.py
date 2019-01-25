cc669b1575886df444c449e11290503562babf23
VincentDerk <vincent.derkinderen@cs.kuleuven.be>
2019-01-25 15:51:00 +0100

    Fix Iterator edge case at root smoothing.

--- tests/test_iterator.py
+++ tests/test_iterator.py
@@ -35,11 +35,11 @@ def test_it1():
     # print(f"mc = {mc}")
     assert mc == 7.0
 
-    it = SddIterator(sdd, smooth=True)
+    it = SddIterator(sdd, smooth=True, smooth_to_root=True)
     mc = it.depth_first(f, SddIterator.func_modelcounting)
     assert mc == 7, "MC {} != 7".format(mc)
 
-    it = SddIterator(sdd, smooth=False)
+    it = SddIterator(sdd, smooth=False, smooth_to_root=True)
     mc = it.depth_first(f, SddIterator.func_modelcounting)
     assert mc == 3, "MC (non-smooth) {} != 3".format(mc)
 
@@ -56,13 +56,28 @@ def test_it2():
     # print(f"mc = {mc}")
     assert mc == 16.0
 
-    it = SddIterator(sdd, smooth=True)
+    it = SddIterator(sdd, smooth=True, smooth_to_root=True)
     mc = it.depth_first(f, SddIterator.func_modelcounting)
     assert mc == 16, "MC {} != 16".format(mc)
 
-    it = SddIterator(sdd, smooth=False)
+    it = SddIterator(sdd, smooth=False, smooth_to_root=True)
     mc = it.depth_first(f, SddIterator.func_modelcounting)
     assert mc == 1, "MC (non-smooth) {} != 1".format(mc)
+
+    f = (a & -a)  # = SddNode(False)
+
+    wmc = f.wmc(log_mode=False)
+    mc = wmc.propagate()
+    # print(f"mc = {mc}")
+    assert mc == 0.0
+
+    it = SddIterator(sdd, smooth=True, smooth_to_root=True)
+    mc = it.depth_first(f, SddIterator.func_modelcounting)
+    assert mc == 0, "MC {} != 0".format(mc)
+
+    it = SddIterator(sdd, smooth=False, smooth_to_root=True)
+    mc = it.depth_first(f, SddIterator.func_modelcounting)
+    assert mc == 0, "MC (non-smooth) {} != 0".format(mc)
 
 
 def test_it3():
@@ -70,33 +85,33 @@ def test_it3():
     vtree = Vtree(var_count=4, var_order=[1, 2, 3, 4], vtree_type="right")
     sdd = SddManager.from_vtree(vtree)
     a, b, c, d = sdd.vars[:5]
-    f = a  # = SddNode(True)
+    f = a
 
     wmc = f.wmc(log_mode=False)
     mc = wmc.propagate()
     # print(f"mc = {mc}")
     assert mc == 8.0
 
-    it = SddIterator(sdd, smooth=True)
+    it = SddIterator(sdd, smooth=True, smooth_to_root=True)
     mc = it.depth_first(f, SddIterator.func_modelcounting)
     assert mc == 8, "MC {} != 8".format(mc)
 
-    it = SddIterator(sdd, smooth=False)
+    it = SddIterator(sdd, smooth=False, smooth_to_root=True)
     mc = it.depth_first(f, SddIterator.func_modelcounting)
     assert mc == 1, "MC (non-smooth) {} != 1".format(mc)
 
-    f = -a  # = SddNode(True)
+    f = -a
 
     wmc = f.wmc(log_mode=False)
     mc = wmc.propagate()
     # print(f"mc = {mc}")
     assert mc == 8.0
 
-    it = SddIterator(sdd, smooth=True)
+    it = SddIterator(sdd, smooth=True, smooth_to_root=True)
     mc = it.depth_first(f, SddIterator.func_modelcounting)
     assert mc == 8, "MC {} != 8".format(mc)
 
-    it = SddIterator(sdd, smooth=False)
+    it = SddIterator(sdd, smooth=False, smooth_to_root=True)
     mc = it.depth_first(f, SddIterator.func_modelcounting)
     assert mc == 1, "MC (non-smooth) {} != 1".format(mc)
 
@@ -136,7 +151,7 @@ if __name__ == "__main__":
     logger.addHandler(sh)
     directory = Path(os.environ.get('TESTDIR', Path(__file__).parent))
     print(f"Saving files to {directory}")
-    # test_it1()
-    # test_it2()
-    # test_it3()
+    test_it1()
+    test_it2()
+    test_it3()
     test_it4()
