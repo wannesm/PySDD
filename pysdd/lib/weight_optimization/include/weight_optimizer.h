57ccc22c4720ece54f11a593900e0a885023c6cc
Jessa Bekker <jessa.bekker@gmail.com>
2021-01-22 18:48:36 +0100

    remove unnecessary counts_fixed and improve comments to make it clearer and more general

--- pysdd/lib/weight_optimization/include/weight_optimizer.h
+++ pysdd/lib/weight_optimization/include/weight_optimizer.h
@@ -10,12 +10,11 @@ typedef struct WeightOptimizationProblem WeightOptimizationProblem;
 struct WeightOptimizationProblem {
 	SddNode* sdd;
 	SddManager* mgr;
-	int m_instances;
-	int n_optimize; // num_features to optimize
-	int* ind_optimize;
-	double* weights_optimize;
-	int* counts_optimize;
-	long double ll_fix;
+	int nb_instances;
+	int n; // num_features to optimize
+	int* lits;
+	double* weights;
+	int* counts;
 	WmcManager* wmcManager;
 	long double loglikelihood;
 };
@@ -47,8 +46,8 @@ int _progress(void* instance, const lbfgsfloatval_t *x,
 
 // constructor weightOptimization
 WeightOptimizationProblem wop_new(SddNode* sdd, SddManager* mgr, int m_instances,
-	        int n_optimize, int* ind_optimize,  double* weights_optimize, int* counts_optimize,
-	        int n_fix, int* ind_fix,  double* weights_fix, int* counts_fix);
+	        int n_optimize, int* lits_optimize,  double* weights_optimize, int* counts_optimize,
+	        int n_fix, int* lits_fix,  double* weights_fix);
 
 void wop_free(WeightOptimizationProblem* wop);
 
@@ -71,22 +70,44 @@ lbfgsfloatval_t wo_prior(WeightOptimizer* wo, const lbfgsfloatval_t* cur_weights
 
 
 /*
-    n, ind, weights and counts input parameters come for optimize and fix.
-     - optimize refers to the weights to be optimized
-     - fix refers to the weights that should be used when evaluating the model, but are to remain fixed
+    Optimize the SDD literal log weights to maximize the log likelihood of the weights for some data
+    using l-BFGS optimization.
+    I.e. optimize the probabilistic model encoded by the SDD: https://doi.org/10.1016/j.artint.2007.11.002
 
-    n: number of weights
-    ind: array of length n with the indicators that the weights/counts refer to
-    weights: feature weights in model. weights[i] is the weight of feature ind[i]
-    counts: feature counts in data. counts[i] is the count of feature ind[i]
+    The data is characterized by the number of instances (m) and the counts of instances in which the literals of the
+    to-be-optimized weights are true.
 
-    max(n)<= sdd_manager_var_count(mgr)
+    Not all literal weights need to be optimized.
+    The weights of the literals that are not optimized are by default set to log(1)=0, so that they do not affect the LL.
+    Alternatively, fixed weights can be provided for literals through lits_fix and weights_fix.
 
-    indicators that are not in ind_fix nor ind_optimize are not used, i.e. both their literal weights are set to 0.
+
+    preconditions:
+     - 1 <= abs(lit)) <= sdd_manager_var_count(mgr) for lit in lits_optimize and lits_fix
+     - lits_optimize & lits_fix = {}  (intersection of lits_optimize and lits_fix is empty)
+
+    params:
+    - *SddNode sdd: SDD
+    - *SddManager mgr: SDD manager
+    - int m_instances: The number of instances in the data
+    - int n_optimize: The number of weights to be optimized
+    - int[n_optimize] lits_optimize: The literals whose weights are to be optimized.
+    - int[n_optimize] counts_optimize: counts_optimize[i] is the count of instances in which literal lits_optimize[i] is true.
+    - double[n_optimize] weights_optimize: weights_optimize[i] is the initial weight for literal lits_optimize[i]
+    - int n_fix: The number of weights to be fixed (to other values than log(1)=0)
+    - int[n_fix] lits_fix: The literals whose weights are to be fixed
+    - double[n_fix] weights_fix: weights_fix[i] is the fixed weight for literal lits_fix[i]
+    - long double prior_sigma: sigma of gaussian prior on weights
+    - long double l1_const: strength of l1 regularisation
+    - int max_iter: maximum number of l-bfgs iterations
+    - long double delta: delta for convergence test in l-bfgs, it determines the minimum rate of decrease of the objective function
+    - long double epsilon: distance for delta-based convergence test in l-bfgs
+
+    result: The weights in weight_optimize are now optimized
 */
 void optimize_weights(SddNode* sdd, SddManager* mgr, int m_instances,
-        int n_optimize, int* ind_optimize,  double* weights_optimize, int* counts_optimize,
-        int n_fix, int* ind_fix,  double* weights_fix, int* counts_fix,
+        int n_optimize, int* lits_optimize,  double* weights_optimize, int* counts_optimize,
+        int n_fix, int* lits_fix,  double* weights_fix,
         long double prior_sigma, long double l1_const, int max_iter, long double delta, long double epsilon);
 
 
--- pysdd/lib/weight_optimization/src/weight_optimizer.c
+++ pysdd/lib/weight_optimization/src/weight_optimizer.c
@@ -33,30 +33,26 @@ int _progress(void* instance, const lbfgsfloatval_t *x,
 }
 
 WeightOptimizationProblem wop_new(SddNode* sdd, SddManager* mgr, int m_instances,
-	        int n_optimize, int* ind_optimize,  double* weights_optimize, int* counts_optimize,
-	        int n_fix, int* ind_fix,  double* weights_fix, int* counts_fix ){
+	        int n_optimize, int* lits_optimize,  double* weights_optimize, int* counts_optimize,
+	        int n_fix, int* lits_fix,  double* weights_fix){
 
     WeightOptimizationProblem wop;
 
 	wop.sdd = sdd;
 	wop.mgr = mgr;
-	wop.m_instances = m_instances;
-	wop.n_optimize = n_optimize;
-	wop.ind_optimize = ind_optimize;
-	wop.weights_optimize = weights_optimize;
-	wop.counts_optimize = counts_optimize;
-	wop.loglikelihood = INFINITY;
+	wop.nb_instances = m_instances;
+	wop.n = n_optimize;
+	wop.lits = lits_optimize;
+	wop.weights = weights_optimize;
+	wop.counts = counts_optimize;
 
-    // pre-calculate the ll contribution of the fixed features
-	long double ll_fix = 0;
-	for (int i= 0; i <n_fix; i++) {
-	    ll_fix += (counts_fix[i] * weights_fix[i]);
-	}
-	wop.ll_fix = ll_fix;
-
+    // Initialize weighted model counter in log mode.
+    // The neutral weight in log mode is log(1)=0
     wop.wmcManager = wmc_manager_new(sdd, 1, mgr);
 
-    // set all literal weights to one, so that unused variables are ignored
+    // set all literal weights to the neutral weight log(1)=0
+    // Literals that are not set later (by fixing them or optimizing them) will thus have this weight,
+    // meaning that these literal weights doe not affect the LL
 	for (int i = 1; i <= sdd_manager_var_count(mgr); i++) {
 		wmc_set_literal_weight(i, wmc_one_weight(wop.wmcManager), wop.wmcManager);
 		wmc_set_literal_weight(-i, wmc_one_weight(wop.wmcManager), wop.wmcManager);
@@ -64,7 +60,7 @@ WeightOptimizationProblem wop_new(SddNode* sdd, SddManager* mgr, int m_instances
 
     // set literal weights of indicators to be fixed to their fixed weights
 	for (int i = 1; i <= n_fix; i++) {
-		wmc_set_literal_weight(ind_fix[i], weights_fix[i], wop.wmcManager);
+		wmc_set_literal_weight(lits_fix[i], weights_fix[i], wop.wmcManager);
 	}
 
 	return wop;
@@ -78,50 +74,79 @@ void wop_free(WeightOptimizationProblem* wop) {
 
 SddWmc wop_update_wmc(WeightOptimizationProblem* wop, const lbfgsfloatval_t* cur_weights) {
     // set optimized weights from cur_weights
-	for (int i= 0; i <wop->n_optimize; i++) {
-	    wmc_set_literal_weight(wop->ind_optimize[i], cur_weights[i], wop->wmcManager);
+	for (int i= 0; i <wop->n; i++) {
+	    wmc_set_literal_weight(wop->lits[i], cur_weights[i], wop->wmcManager);
 	}
 
-	// compute WMC
+	// compute partition function as the weighted model count
 	SddWmc log_wmc = wmc_propagate(wop->wmcManager);
 	assert(isfinite(log_wmc));
 
 	return log_wmc;
 }
 
+/*
+Infer the negative log likelihood of the model, given the example counts
+
+Derivation of log likelihood calculation:
+
+    weight(example) = product_k weight_k ^ literal_k_active_in_example
+    log(weight(example) = sum_k log(weight_k) * literal_k_active_in_example
+
+    Pr(example) = weight(example)/Z
+    Z = partition function = sum_world weight(world)  (sum of the weights of all possible worlds)
+
+    log( Pr(example)) = sum_k log(weight_k) * literal_k_active_in_example - log(Z)
+
+    log LL = sum_examples log( Pr(example))
+       = sum_examples sum_k log(weight_k) * literal_k_active_in_example - log(Z)
+       = sum_k log(weight_k) * count_examples(literal_k_active_in_example) - log(Z)*nb_instances
+
+    -log LL = log(Z)*nb_instances - sum_k log(weight_k) * count_examples(literal_k_active_in_example)
+
+    Literals with log(weight)=log(1)=0 do not contribute to the LL, so they can be ignored in the calculation
+
+In the implementation, the contribution of the literals with fixed weights is not added to the log likelihood.
+   ( sum_k_fixed log(weight_k_fixed) * count_examples(literal_k_fixed_active_in_example) )
+Therefore, when fixed weights are used, the log likelihood is incorrect, but with a  constant difference.
+Minimizing this number is equivalent to minimizing the correct log likelihood
+*/
 long double wop_infer(WeightOptimizationProblem* wop, const lbfgsfloatval_t* cur_weights, SddWmc log_wmc) {
 
-    // partition function
-	long double negll = wop->m_instances * log_wmc;
-
-    // contribution of fixed features to loglikelihood
-	negll -= wop->ll_fix;
+    // log partition function
+	long double negll = wop->nb_instances * log_wmc;
 
     // contribution optimized features to loglikelihood
-	for (int i= 0; i <wop->n_optimize; i++) {
-	    negll -= (wop->counts_optimize[i] * cur_weights[i]);
+	for (int i= 0; i <wop->n; i++) {
+	    negll -= (wop->counts[i] * cur_weights[i]);
 	}
 
 	assert(negll >= -1e-6);
 	return negll;
 }
 
+/*
+The gradient is the negative difference between the literal probability in the model and the data.
+https://dl.acm.org/doi/10.5555/2283516.2283536
+*/
 void wop_gradient(WeightOptimizationProblem* wop, lbfgsfloatval_t* gradient) {
 
-	for (int i= 0; i <wop->n_optimize; i++) {
+	for (int i= 0; i <wop->n; i++) {
 		long double marginalProb = logProb2Prob(
-				wmc_literal_pr(wop->ind_optimize[i], wop->wmcManager));
+				wmc_literal_pr(wop->lits[i], wop->wmcManager));
 		assert(isfinite(marginalProb));
 		assert(marginalProb<=1+1e-6);
 		assert(marginalProb>=-1e-6);
 
-		gradient[i] = -(wop->counts_optimize[i] - marginalProb * wop->m_instances);
+		gradient[i] = -(wop->counts[i] - marginalProb * wop->nb_instances);
 
 		assert(isfinite(gradient[i]));
 	}
 }
 
-// calculate likelihood and gradient of the weights to be optimized
+/*
+calculate likelihood and gradient of the weights to be optimized
+*/
 lbfgsfloatval_t wop_evaluate(WeightOptimizationProblem* wop,
         const lbfgsfloatval_t* cur_weights, lbfgsfloatval_t* gradient) {
 
@@ -133,14 +158,13 @@ lbfgsfloatval_t wop_evaluate(WeightOptimizationProblem* wop,
 }
 
 /*
-When optimization is done: copy last weights and final score to wop
+When optimization is done: copy optimized weights to wop
 */
 void wop_done(WeightOptimizationProblem* wop, lbfgsfloatval_t *m_x,
 		lbfgsfloatval_t fx) {
-	for (int i = 0; i < wop->n_optimize; i++) {
-		wop->weights_optimize[i] = (double) m_x[i];
+	for (int i = 0; i < wop->n; i++) {
+		wop->weights[i] = (double) m_x[i];
 	}
-	wop->loglikelihood = -fx / wop->m_instances;
 }
 
 WeightOptimizer wo_new(long double prior_sigma, long double l1_const, int max_iter,
@@ -161,18 +185,18 @@ int wo_optimize(WeightOptimizer* wo, WeightOptimizationProblem* problem) {
 
 	/* Initialize the variables and settings. */
 	lbfgsfloatval_t fx;
-	lbfgsfloatval_t *m_x = lbfgs_malloc(problem->n_optimize);
+	lbfgsfloatval_t *m_x = lbfgs_malloc(problem->n);
 	if (m_x == NULL) {
 		printf("ERROR: Failed to allocate a memory block for variables.\n");
 		return 1;
 	}
-	for (int i = 0; i < problem->n_optimize; i++) {
-		m_x[i] = (lbfgsfloatval_t) problem->weights_optimize[i];
+	for (int i = 0; i < problem->n; i++) {
+		m_x[i] = (lbfgsfloatval_t) problem->weights[i];
 	}
 	if (wo->settings.orthantwise_c != 0) {
 		wo->settings.linesearch = LBFGS_LINESEARCH_BACKTRACKING; //required by lbfgs lib
 		wo->settings.orthantwise_start = 0; //problem->nbUnitFeatures;
-		wo->settings.orthantwise_end = problem->n_optimize; // incorporate all parameters for non-unit features in L1 norm
+		wo->settings.orthantwise_end = problem->n; // incorporate all parameters for non-unit features in L1 norm
 	}
 
 	/*
@@ -182,7 +206,7 @@ int wo_optimize(WeightOptimizer* wo, WeightOptimizationProblem* problem) {
 	struct OptimizationRun run;
 	run.problem = problem;
 	run.learner = wo;
-	int ret = lbfgs(problem->n_optimize, m_x, &fx, _evaluate, _progress, &run,
+	int ret = lbfgs(problem->n, m_x, &fx, _evaluate, _progress, &run,
 			&wo->settings);
 
 	wop_done(problem, m_x, fx);
@@ -190,6 +214,9 @@ int wo_optimize(WeightOptimizer* wo, WeightOptimizationProblem* problem) {
 	return ret;
 }
 
+/*
+Calculate the gaussian prior on the weights and add it to the gradient
+*/
 lbfgsfloatval_t wo_prior(WeightOptimizer* wo, const lbfgsfloatval_t* cur_weights,
 		lbfgsfloatval_t *gradient, const int n) {
 
@@ -212,27 +239,16 @@ lbfgsfloatval_t wo_prior(WeightOptimizer* wo, const lbfgsfloatval_t* cur_weights
 
 
 /*
-    n, ind, weights and counts input parameters come for optimize and fix.
-     - optimize refers to the weights to be optimized
-     - fix refers to the weights that should be used when evaluating the model, but are to remain fixed
-
-    n: number of weights
-    ind: array of length n with the indicators that the weights/counts refer to
-    weights: feature weights in model. weights[i] is the weight of feature ind[i]
-    counts: feature counts in data. counts[i] is the count of feature ind[i]
-
-    max(n)<= sdd_manager_var_count(mgr)
-
-    indicators that are not in ind_fix nor ind_optimize are not used, i.e. both their literal weights are set to 0.
+Doc in weight_optimizer.h
 */
 void optimize_weights(SddNode* sdd, SddManager* mgr, int m_instances,
-        int n_optimize, int* ind_optimize,  double* weights_optimize, int* counts_optimize,
-        int n_fix, int* ind_fix,  double* weights_fix, int* counts_fix,
+        int n_optimize, int* lits_optimize,  double* weights_optimize, int* counts_optimize,
+        int n_fix, int* lits_fix,  double* weights_fix,
         long double prior_sigma, long double l1_const, int max_iter, long double delta, long double epsilon) {
 
 	WeightOptimizationProblem wop = wop_new(sdd, mgr, m_instances,
-	        n_optimize, ind_optimize,  weights_optimize, counts_optimize,
-	        n_fix, ind_fix, weights_fix, counts_fix);
+	        n_optimize, lits_optimize,  weights_optimize, counts_optimize,
+	        n_fix, lits_fix, weights_fix);
 
     WeightOptimizer wo = wo_new(prior_sigma, l1_const, max_iter, delta, epsilon);
 
