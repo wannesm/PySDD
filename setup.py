d0f81eca1de6c5490540f08044a65bbc24cbc548
Robin Manhaeve <robin.manhaeve@kuleuven.be>
2024-10-25 23:19:19 +0200

    remove resolve

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ sdd_version = "2.0"
 
 libwrapper_path = here / "pysdd" / "lib"
 sdd_path = libwrapper_path / f"sdd-{sdd_version}"
-lib_path = (sdd_path / "lib").resolve()
+lib_path = sdd_path / "lib"
 inc_path = sdd_path / "include"
 src_path = sdd_path / "src"
 csrc_path = here / "pysdd" / "src"
